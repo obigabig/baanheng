bece0a9476a0342b2dfb9218360efeb12a301cf8
obigabig <obigabig.g@gmail.com>
2018-08-17 10:45:11 +0700

    V.0.2.0

--- client/src/const/index.js
+++ client/src/const/index.js
@@ -1,7 +1,6 @@
 
-exports.facebookClientId = "505475596589543"
-exports.googleClientId = "1086485022003-6mp8hgbp4govp1qt6ghg1ag4lcv3uitv.apps.googleusercontent.com"
-
+exports.facebookClientId = process.env.FB_CLIENT_ID
+exports.googleClientId = process.env.GOOGLE_CLIENT_ID
 
 const contractStatusValue = {
     new: 'งานใหม่',
